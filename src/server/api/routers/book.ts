f3ec9bfcfa00d8eefcfcf28d522b897d64b6a423
Vinícius Ventura <58307012+viniciusventura29@users.noreply.github.com>
2023-01-19 17:11:19 +0000

    📦 NEW: Creating book on book router

--- src/server/api/routers/book.ts
+++ src/server/api/routers/book.ts
@@ -1,22 +1,32 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const bookRouter = createTRPCRouter({
-    createBook: publicProcedure
+    createBook: protectedProcedure
     .input(z.object({
         name: z.string(),
         description: z.string(),
-        body: z.string().optional(),
+        body: z.string().optional().default(''),
         author: z.string(),
         category: z.string()
     }))
-    .mutation(({input})=>{
-        return input
+    .mutation(async({ctx, input})=>{
+        const books = await ctx.prisma.book.create({
+            data:{
+                name: input.name,
+                description: input.description,
+                body: input.body,
+                userId: input.author,
+                category: input.category,
+            },
+        })
+        return books
     }),
 
     getAll: publicProcedure
     .query(({ctx})=>{
+        
         return ctx.prisma.book.findMany();
-    })
+    }),
     
 })
