d9181078411ca054d531d98e1bb9b0f205763d23
Vinícius Ventura <58307012+viniciusventura29@users.noreply.github.com>
2023-01-19 13:55:43 +0000

    📦 NEW: Modal open & close working & creating book

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,15 @@
-import { type NextPage } from "next";
 import { useState } from "react";
+import { AddItemModal } from "./components/AddItemModal";
 
 import { api } from "../utils/api";
 
-const Home: NextPage = () => {
+interface modalProps{
+    state: boolean
+}
+
+function Home ({state}: modalProps) {
   const [modalOpen, setModalOpen]= useState(false)
-  const [name, setName]= useState('')
-  const [category, setCategory]= useState('')
-  const [description, setDescription]= useState('')
-
-  function save(){
-    const book = api.book.createBook.useQuery({
-      name: name,
-      author: "oi",
-      category: category,
-      description: description,
-    });
-  }
-
-  const AddItem: any =()=>{
-    if (modalOpen === true){
-    return <div className="absolute z-50 bg-slate-500 p-5 flex flex-col gap-2 w-96 h-96">
-      <input className="rounded h-7" type="text" onChange={(e)=>setName(e)} />
-      <input className="rounded h-7" type="text" onChange={(e)=>setCategory(e)} />
-      <input className="rounded h-7" type="text" value={description} onChange={(e)=>setDescription(e)} />
-      <button className="p-2 bg-slate-400 rounded-md mt-5" onClick={save}>save</button>
-    </div>;
-    }
-    else{
-      null
-    }
-  }
+  const books = api.book.getAll.useQuery();
 
   return (
     <>
@@ -38,7 +17,8 @@ const Home: NextPage = () => {
         <button className="rounded bg-slate-300 p-2" onClick={()=>setModalOpen(!modalOpen)}>
           Add item +
         </button>
-        <AddItem />
+        {/* This is wrong for now <div>{books.data?.map((book)=>book.name)}</div> */}
+        {modalOpen ? <AddItemModal /> : null}
       </div>
     </>
   );
