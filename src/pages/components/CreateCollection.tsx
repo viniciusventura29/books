f41b9d2baabe4169c698482a98a38e45d30ba3f4
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-26 16:46:05 -0300

    📦 NEW: Create collection component not finished

---
+++ src/pages/components/CreateCollection.tsx
@@ -0,0 +1,68 @@
+import { useSession } from "next-auth/react";
+import { FormEvent, useState } from "react";
+import { api } from "../../utils/api";
+
+export default function CreateCollection() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const { mutate } = api.book.createBook.useMutation();
+  const { data: sessionData } = useSession();
+  const [modal, setModal] = useState(false)
+
+  const createBook = (e: FormEvent) => {
+    console.log(sessionData);
+    console.log(sessionData?.user?.id);
+    e.preventDefault();
+    mutate({
+      name,
+      description,
+      category,
+      userId: "clddd4cx30000ht3c6rqpmj9o",
+    });
+  };
+
+  return (
+    <div className="mt-2 w-2/3 items-stretch rounded bg-slate-200 p-5">
+      <form
+        onSubmit={createBook}
+        className="space-x-3"
+      >
+        <input
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+          type="text"
+          placeholder="title"
+          className="rounded border-2 border-gray-600 p-2 transition duration-700 ease-in-out dark:bg-slate-800 dark:text-gray-200"
+        />
+        <input
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          value={description}
+          type="text"
+          placeholder="description"
+          className="rounded border-2 border-gray-600 p-2 transition duration-700 ease-in-out dark:bg-slate-800 dark:text-gray-200"
+        />
+        <input
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+          value={category}
+          type="text"
+          placeholder="category"
+          className="rounded border-2 border-gray-600 p-2 transition duration-700 ease-in-out dark:bg-slate-800 dark:text-gray-200"
+        />
+        <button
+          type="submit"
+          className="rounded bg-blue-500 p-2 px-10 text-white duration-500 hover:bg-blue-600"
+        >
+          Add +
+        </button>
+        <button type="submit" onClick={()=>setModal(false)} className="h-10 border-2 rounded border-red-500 p-1 px-10 text-red-600 duration-500 hover:bg-red-100">Cancel</button>
+      </form>
+    </div>
+  );
+}
