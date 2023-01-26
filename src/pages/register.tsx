29d6f444ec068f889f0ab36e2d10b7841adcbdeb
viniciusventura29 <viniciusventura29@gmail.com>
2023-01-25 21:21:57 -0300

    🐛 FIX: Function name

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -7,7 +7,7 @@ export default function Register (){
     const [password, setPassword] = useState('')
     const {mutate} = api.user.createUser.useMutation();
 
-    const login =()=>[
+    const register =()=>[
         mutate({
             name,
             email,
@@ -23,7 +23,7 @@ export default function Register (){
                   <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                       Create your account
                   </h1>
-                  <form className="space-y-4 md:space-y-6" onSubmit={login}>
+                  <form className="space-y-4 md:space-y-6" onSubmit={register}>
                   <div>
                           <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
                           <input onChange={(e)=>{setName(e.target.value)}} value={name} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" />
