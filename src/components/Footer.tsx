ea8a65e77c23756c71c0bf358933a615eac4f9c2
viniciusventura29 <viniciusventura29@gmail.com>
2023-02-14 16:53:08 -0300

    📦 NEW: Adding link to my github perfil in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link"
+
 export const Footer = () =>{
     return(
-        <footer className="flex gap-2 w-full mx-auto pb-10 pt-28 justify-center">Developed by <span className="font-semibold">Vinicius Ventura</span></footer>
+        <footer className="flex gap-2 w-full mx-auto pb-10 pt-28 justify-center ">Developed by <Link href='https://github.com/viniciusventura29' className="hover:text-purple-900 font-semibold">Vinicius Ventura</Link></footer>
     )
 }
