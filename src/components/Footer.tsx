da7c0d71316e7c34f9d0caae20df6eda703ce3df
viniciusventura29 <viniciusventura29@gmail.com>
2023-02-14 15:46:40 -0300

    📦 NEW: Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () =>{
+    return(
+        <footer className="flex gap-2 w-full mx-auto pb-10 pt-28 justify-center">Developed by <span className="font-semibold">Vinicius Ventura</span></footer>
+    )
+}
