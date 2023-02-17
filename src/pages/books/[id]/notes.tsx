ef6b9c30672b660ea38e4c8945d9122b5d43ffcd
viniciusventura29 <viniciusventura29@gmail.com>
2023-02-17 16:46:35 -0300

    👌 IMPROVE: Modal side to create a note

--- src/pages/books/[id]/notes.tsx
+++ src/pages/books/[id]/notes.tsx
@@ -1,21 +1,26 @@
 import { GetServerSidePropsContext } from "next";
-import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { api } from "../../../utils/api";
 import { Breadcrumb } from "../../../components/Breadcrumb";
 import { IconPencil, IconPlus } from "@tabler/icons-react";
 
 interface INote {
-  id: string
-  title: string
-  body: string
-  updatedAt: string
-  color: string
+  id: string;
+  title: string;
+  body: string;
+  updatedAt: string;
+  color: string;
 }
 
-interface IModalSide{
-    openSideModal: boolean
-    setOpenSideModal: Dispatch<SetStateAction<boolean>>
-
+interface IModalSide {
+  openSideModal: boolean;
+  setOpenSideModal: Dispatch<SetStateAction<boolean>>;
 }
 
 export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
@@ -26,97 +31,116 @@ export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
 
 const SingleNote = (note: INote) => {
   return (
-    <div className="w-full h-64 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
+    <div className="mb-6 flex h-64 w-full flex-col justify-between rounded-lg border border-gray-400 bg-white py-5 px-4 dark:border-gray-700 dark:bg-gray-800">
       <div>
-        <h4 className="text-gray-800 dark:text-gray-100 font-bold mb-3">{note.title}</h4>
-        <p className="text-gray-800 dark:text-gray-100 text-sm">{note.body}</p>
+        <h4 className="mb-3 font-bold text-gray-800 dark:text-gray-100">
+          {note.title}
+        </h4>
+        <p className="text-sm text-gray-800 dark:text-gray-100">{note.body}</p>
       </div>
       <div>
         <div className="flex items-center justify-between text-gray-800 dark:text-gray-100">
           <p className="text-sm">{note.updatedAt}</p>
-          <button className="w-8 h-8 rounded-full bg-gray-800 dark:bg-gray-100 dark:text-gray-800 text-white flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-black" aria-label="edit note" role="button">
+          <button
+            className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 dark:bg-gray-100  dark:text-gray-800"
+            aria-label="edit note"
+            role="button"
+          >
             <IconPencil size={20} />
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const SideModal = (modalSideProps: IModalSide) => {
-  console.log(modalSideProps.openSideModal)
+  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
+
   return (
-    <div className={`${modalSideProps.openSideModal ? 'visible opacity-100 ' : 'invisible opacity-0'} p-6 backdrop-brightness-50 bg-white shadow-lg absolute min-h-screen right-0 w-1/4 transition-all duration-300`}>
-      <div className="cursor-pointer bg-red-500 hover:bg-red-600 rounded p-2 flex items-center justify-center w-6 h-6 text-white" onClick={()=>modalSideProps.setOpenSideModal(false)}>X</div>
+    <div
+      className={`${
+        modalSideProps.openSideModal
+          ? "visible opacity-100 "
+          : "invisible opacity-0"
+      } absolute right-0 min-h-screen w-1/4 bg-white p-6 shadow-lg backdrop-brightness-50 transition-all duration-300`}
+    >
+      <div
+        className="mb-12 flex h-6 w-6 cursor-pointer items-center justify-center rounded bg-red-500 p-2 text-white hover:bg-red-600"
+        onClick={() => modalSideProps.setOpenSideModal(false)}
+      >
+        X
+      </div>
+      <div className="flex flex-col gap-3">
+        <h2 className="mb-2 text-2xl font-bold">Create Note</h2>
+        <input className="rounded border p-2" type="text" placeholder="Title" onChange={(e)=>setTitle(e.target.value)} />
+        <textarea
+          className="resize-none rounded border p-2"
+          placeholder="Write the body here..."
+          onChange={(e)=>setBody(e.target.value)}
+        ></textarea>
+        <span className="flex items-center gap-2">
+          Color selection:{" "}
+          <input
+            className="h-6 w-6 border-none outline-none"
+            type="color"
+            name=""
+            id=""
+          />{" "}
+        </span>
+        <button className="mt-1 rounded bg-green-600 py-1 text-white hover:bg-green-700">
+          Save
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default function Notes(props: { id: { id: string } }) {
   const util = api.useContext();
   const idBook = props.id.id;
-  const notesList = api.notes.getAllNotes.useQuery({ bookId: idBook })
+  const notesList = api.notes.getAllNotes.useQuery({ bookId: idBook });
 
-  const [body, setBody] = useState<string>("");
-  const [title, setTitle] = useState<string>("");
-
-  const [openSideModal, setOpenSideModal] = useState(false)
-  console.log(openSideModal)
-
-  useEffect(() => {
-    document.addEventListener('keydown', detectKeyDown, true)
-  }, [])
-
-  api.notes.getOneNote.useQuery(
-    {
-      bookId: idBook,
-    },
-    {
-      onSuccess: (updatedNote) => {
-        if (updatedNote?.body) {
-          setBody(updatedNote.body);
-        }
-      },
-    }
-  );
-
-  const updateNoteMutation = api.notes.updateNote.useMutation({
-    onSuccess: () => util.notes.getOneNote.invalidate(),
-  });
-
-  const updateNote = useCallback(() => {
-    updateNoteMutation.mutate({
-      title: title,
-      body: body,
-      bookId: idBook,
-    });
-  }, [title, body, idBook]);
-
-  const detectKeyDown = (e: KeyboardEvent) => {
-    if (e.ctrlKey && e.key === 's' && !e.repeat) {
-      e.preventDefault()
-      updateNote()
-    }
-  }
+  const [openSideModal, setOpenSideModal] = useState(false);
+  console.log(openSideModal);
 
   return (
-    <div className="bg-gray-100 transition duration-700 ease-in-out dark:bg-slate-900 flex flex-col">
+    <div className="flex flex-col bg-gray-100 transition duration-700 ease-in-out dark:bg-slate-900">
       <Breadcrumb idBook={idBook} toolName="Notes" />
-      <div className="mx-auto flex flex-col py-10 min-h-screen w-auto min-w-[75%] max-w-min">
+      <div className="mx-auto flex min-h-screen w-auto min-w-[75%] max-w-min flex-col py-10">
         <h2 className="mb-10 text-3xl font-semibold">Notes</h2>
         <div className="mt-20 grid grid-flow-row grid-cols-4 gap-4">
           {notesList.data?.map((note) => (
-            <SingleNote key={note.id} color="white" title={note.title} updatedAt={note.updatedAt.getDate().toString()} id={note.id} body={note.body} />
+            <SingleNote
+              key={note.id}
+              color="white"
+              title={note.title}
+              updatedAt={note.updatedAt.getDate().toString()}
+              id={note.id}
+              body={note.body}
+            />
           ))}
-          <div onClick={() => setOpenSideModal(true)} className="transition-all duration-200 hover:shadow-lg hover:shadow-purple-500 w-full h-64 opacity-70 flex flex-col justify-between dark:bg-gray-800 bg-white dark:border-gray-700 rounded-lg border border-gray-400 mb-6 py-5 px-4">
+          <div
+            onClick={() => setOpenSideModal(true)}
+            className="mb-6 flex h-64 w-full flex-col justify-between rounded-lg border border-gray-400 bg-white py-5 px-4 opacity-70 transition-all duration-200 hover:shadow-lg hover:shadow-purple-500 dark:border-gray-700 dark:bg-gray-800"
+          >
             <div>
-              <h4 className="text-gray-800 dark:text-gray-100 font-bold mb-3">New note title</h4>
-              <p className="text-gray-800 dark:text-gray-100 text-sm">New note body</p>
+              <h4 className="mb-3 font-bold text-gray-800 dark:text-gray-100">
+                New note title
+              </h4>
+              <p className="text-sm text-gray-800 dark:text-gray-100">
+                New note body
+              </p>
             </div>
             <div>
-              <div className="flex items-center justify-between cursor-pointer text-gray-800 dark:text-gray-100">
+              <div className="flex cursor-pointer items-center justify-between text-gray-800 dark:text-gray-100">
                 <p className="text-sm">New note date</p>
-                <button className="w-8 h-8 rounded-full bg-gray-800 dark:bg-gray-100 dark:text-gray-800 text-white flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-black" aria-label="edit note" role="button">
+                <button
+                  className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 dark:bg-gray-100  dark:text-gray-800"
+                  aria-label="edit note"
+                  role="button"
+                >
                   <IconPlus size={20} />
                 </button>
               </div>
@@ -124,7 +148,10 @@ export default function Notes(props: { id: { id: string } }) {
           </div>
         </div>
       </div>
-      <SideModal openSideModal={openSideModal} setOpenSideModal={setOpenSideModal} />
+      <SideModal
+        openSideModal={openSideModal}
+        setOpenSideModal={setOpenSideModal}
+      />
     </div>
   );
 }
