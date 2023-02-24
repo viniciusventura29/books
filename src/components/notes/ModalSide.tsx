c91405d9e7a97849ec7d7cf1bd105ceda273331f
viniciusventura29 <viniciusventura29@gmail.com>
2023-02-24 11:25:24 -0300

    👌 IMPROVE:  Modalside css and SingleNote css

--- src/components/notes/ModalSide.tsx
+++ src/components/notes/ModalSide.tsx
@@ -4,13 +4,13 @@ import { api } from "../../utils/api";
 interface IModalSide {
   openSideModal: boolean;
   setOpenSideModal: Dispatch<SetStateAction<boolean>>;
-	editing: boolean;
+  editing: boolean;
   bookId: string;
   body?: string;
   title?: string;
   color?: string;
-	NoteId: string;
-	setEditing: Dispatch<SetStateAction<boolean>>;
+  NoteId: string;
+  setEditing: Dispatch<SetStateAction<boolean>>;
 }
 
 export const SideModal = (modalSideProps: IModalSide) => {
@@ -24,7 +24,7 @@ export const SideModal = (modalSideProps: IModalSide) => {
     setBody(modalSideProps.body ? modalSideProps.body : "");
     setTitle(modalSideProps.title ? modalSideProps.title : "");
     setColor(modalSideProps.color ? modalSideProps.color : "");
-  },[modalSideProps.body]);
+  }, [modalSideProps.body]);
 
   const createNote = api.notes.createNote.useMutation({
     onSuccess: async () => {
@@ -32,98 +32,98 @@ export const SideModal = (modalSideProps: IModalSide) => {
       setBody("");
       setTitle("");
       setColor("white");
+      modalSideProps.setOpenSideModal(false)
     },
   });
 
-	const updateNote = api.notes.updateNote.useMutation({
+  const updateNote = api.notes.updateNote.useMutation({
     onSuccess: async () => {
       await util.notes.getAllNotes.invalidate();
       setBody("");
       setTitle("");
-			modalSideProps.setOpenSideModal(false)
-			modalSideProps.setEditing(false)
+      modalSideProps.setOpenSideModal(false)
+      modalSideProps.setEditing(false)
     },
   });
 
   const saveNote = () => {
-		console.log(modalSideProps.editing)
-    if (modalSideProps.editing === true){
-			updateNote.mutate({
-				body,
-				title,
-				noteId: modalSideProps.NoteId,
-				color,
-			})
-		}
-		else{
-			console.log(color)
-			createNote.mutate({
-				bookId: modalSideProps.bookId,
-				title: title,
-				body: body,
-				color: color? color : "white",
-			});
-		}
+    console.log(modalSideProps.editing)
+    if (modalSideProps.editing === true) {
+      updateNote.mutate({
+        body,
+        title,
+        noteId: modalSideProps.NoteId,
+        color,
+      })
+    }
+    else {
+      console.log(color)
+      createNote.mutate({
+        bookId: modalSideProps.bookId,
+        title: title,
+        body: body,
+        color: color ? color : "white",
+      });
+    }
   };
 
   return (
     <div
-      className={`${
-        modalSideProps.openSideModal
+      className={`${modalSideProps.openSideModal
           ? "visible opacity-100 "
           : "invisible opacity-0"
-      } absolute right-0 min-h-screen w-1/4 bg-white p-6 shadow-lg backdrop-brightness-50 transition-all duration-300`}
+        } fixed bg-white right-0 w-1/4 transition-all py-10 p-6 min-h-screen duration-300`}
     >
-      <div
-        className="mb-12 flex h-6 w-6 cursor-pointer items-center justify-center rounded bg-red-500 p-2 text-white hover:bg-red-600"
-        onClick={() => {modalSideProps.setOpenSideModal(false); modalSideProps.setEditing(false)}}
-      >
-        X
-      </div>
-      <div className="flex flex-col gap-3">
-        <h2 className="mb-2 text-2xl font-bold">Create Note</h2>
-        <input
-          value={title}
-          className="rounded border p-2"
-          type="text"
-          placeholder="Title"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <textarea
-          className="resize-none rounded border p-2"
-          placeholder="Write the body here..."
-          onChange={(e) => setBody(e.target.value)}
-          value={body}
-        ></textarea>
-        <span className="flex items-center gap-2">
-          Color selection:
-          <button
-            onClick={() => setColor("yellow-50")}
-            className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-yellow-100"
-          ></button>
-          <button
-            onClick={() => setColor("red-50")}
-            className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-red-100"
-          ></button>
-          <button
-            onClick={() => setColor("green-50")}
-            className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-green-100"
-          ></button>
-          <button
-            onClick={() => setColor("purple-50")}
-            className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-purple-100"
-          ></button>
-          <button
-            onClick={() => setColor("white")}
-            className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-white"
-          ></button>
-        </span>
-        <button
-          onClick={saveNote}
-          className="mt-1 rounded bg-green-600 py-1 text-white hover:bg-green-700"
+        <div
+          className="mb-12 flex h-6 w-6 cursor-pointer items-center justify-center rounded bg-red-500 p-2 text-white hover:bg-red-600"
+          onClick={() => { modalSideProps.setOpenSideModal(false); modalSideProps.setEditing(false) }}
         >
-          Save
-        </button>
+          X
+        </div>
+        <div className="flex flex-col gap-3">
+          <h2 className="mb-2 text-2xl font-bold">Create Note</h2>
+          <input
+            value={title}
+            className="rounded border p-2"
+            type="text"
+            placeholder="Title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            className="resize-none rounded border p-2"
+            placeholder="Write the body here..."
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+          <span className="flex items-center gap-2">
+            Color selection:
+            <button
+              onClick={() => setColor("yellow-50")}
+              className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-yellow-100"
+            ></button>
+            <button
+              onClick={() => setColor("red-50")}
+              className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-red-100"
+            ></button>
+            <button
+              onClick={() => setColor("green-50")}
+              className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-green-100"
+            ></button>
+            <button
+              onClick={() => setColor("purple-50")}
+              className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-purple-100"
+            ></button>
+            <button
+              onClick={() => setColor("white")}
+              className="mt-[2px] h-4 w-4 rounded-full border-[1px] border-gray-700 bg-white"
+            ></button>
+          </span>
+          <button
+            onClick={saveNote}
+            className="mt-1 rounded bg-green-600 py-1 text-white hover:bg-green-700"
+          >
+            Save
+          </button>
       </div>
     </div>
   );
--- src/components/notes/SingleNote.tsx
+++ src/components/notes/SingleNote.tsx
@@ -1,6 +1,7 @@
-import { IconPencil, IconTrash } from "@tabler/icons-react";
-import { Dispatch, SetStateAction, useState } from "react";
+import { IconPencil } from "@tabler/icons-react";
+import { Dispatch, SetStateAction } from "react";
 import { api } from "../../utils/api";
+import { IconX } from "@tabler/icons-react";
 
 interface INote {
   id: string;
@@ -28,7 +29,7 @@ export const SingleNote = (note: INote) => {
 
   return (
     <div
-      className={`mb-6 flex h-64 w-full flex-col justify-between rounded-lg border shadow-lg bg-${note.color} py-5 px-4 dark:border-gray-700 dark:bg-gray-800`}
+      className={`relative mb-6 flex h-64 w-full flex-col justify-between rounded-lg border shadow-lg bg-${note.color} py-5 px-4 dark:border-gray-700 dark:bg-gray-800`}
     >
       <div className="h-full">
         <h4 className="mb-3 font-bold text-gray-800 dark:text-gray-100">
@@ -46,11 +47,11 @@ export const SingleNote = (note: INote) => {
           <div className="flex gap-2">
             <button
               onClick={() => deleteNote.mutate({ id: note.id })}
-              className="flex h-8 w-8 items-center justify-center rounded-full bg-red-600 text-white focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 dark:bg-gray-100  dark:text-gray-800"
+              className="top-4 absolute flex h-8 w-8 items-center justify-center rounded-full hover:bg-red-200  text-red-600 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 dark:bg-gray-100  dark:text-gray-800"
               aria-label="edit note"
               role="button"
             >
-              <IconTrash size={20} />
+              <IconX size={20} />
             </button>
             <button
               onClick={() => {
@@ -61,7 +62,7 @@ export const SingleNote = (note: INote) => {
                 note.setNoteId(note.id);
                 note.setEditing(true)
               }}
-              className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 dark:bg-gray-100  dark:text-gray-800"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-700 hover:bg-gray-800 transition-all duration-200 text-white focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 dark:bg-gray-100  dark:text-gray-800"
               aria-label="edit note"
               role="button"
             >
