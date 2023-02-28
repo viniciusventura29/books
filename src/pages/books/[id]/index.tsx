dee32a61b752609f18a2522db5742e50fe7d97d0
viniciusventura29 <viniciusventura29@gmail.com>
2023-02-28 20:24:25 -0300

    📦 NEW: There is any data in notes or todo

--- src/pages/books/[id]/index.tsx
+++ src/pages/books/[id]/index.tsx
@@ -5,6 +5,7 @@ import { Breadcrumb } from "../../../components/global/Breadcrumb";
 import { Sidebar } from "../../../components/global/Sidebar";
 import { IconChevronRight } from "@tabler/icons-react";
 import { ToDoTask } from "../../../components/todo/TodoTask";
+import { useEffect, useState } from "react";
 
 export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
   const id = ctx.query;
@@ -17,11 +18,46 @@ export default function TasksBooks(props: { id: { id: string } }) {
   const notesList = api.notes.getAllNotes.useQuery({ bookId: idBook });
   const todoList = api.toDo.getAll.useQuery({ bookId: idBook });
 
+  const [thereMoreTodo,setThereMoreTodo] = useState(false)
+  const [thereNoTodo, setThereNoTodo] = useState(false)
+
+  const [thereMoreNotes,setThereMoreNotes] = useState(false)
+  const [thereNoNote, setThereNoNote] = useState(false)
+
+  useEffect(()=>{
+    if (todoList.data === undefined || todoList.data?.length === 0){
+      setThereNoTodo(true)
+    }
+    if (todoList.data?.length !== 0){
+      setThereNoTodo(true)
+    }
+    else if (todoList.data?.length > 5){
+      setThereMoreTodo(true)
+    }
+    else if (todoList.data?.length <= 5){
+      setThereMoreTodo(false)
+    }
+
+    if (notesList.data === undefined || notesList.data?.length === 0){
+      setThereNoNote(true)
+    }
+    else if (notesList.data?.length !== 0){
+      setThereNoNote(false)
+    }
+    else if (notesList.data?.length > 5){
+      setThereMoreNotes(true)
+    }
+    else if (notesList.data?.length <= 5){
+      setThereMoreNotes(false)
+    }
+
+  },[todoList])
+
   return (
     <div className="flex flex-col bg-gray-100 transition duration-700 ease-in-out dark:bg-slate-900">
       <Breadcrumb idBook={idBook} toolName="" />
 
-      <div className="mx-auto flex min-h-screen w-auto 2xl:min-w-[65%] min-w-[55%] max-w-min flex-col gap-20 py-10">
+      <div className="mx-auto flex min-h-screen w-auto 2xl:min-w-[70%] min-w-[65%] max-w-min flex-col gap-20 py-10">
         <section>
           <Link
             href={{ pathname: "/books/[id]/notes", query: { id: idBook } }}
@@ -29,7 +65,7 @@ export default function TasksBooks(props: { id: { id: string } }) {
           >
             <h2 className="text-2xl ">Your notes</h2>
           </Link>
-          <div className="relative grid h-64 grid-flow-row grid-cols-4 gap-4 hover:shadow-purple-500 hover:drop-shadow-lg">
+          <div className="relative grid h-64 grid-flow-row grid-cols-4 gap-2 hover:shadow-purple-500 hover:drop-shadow-lg">
             {notesList.data?.slice(0, 4).map((note) => (
               <div
                 key={note.id}
@@ -55,6 +91,8 @@ export default function TasksBooks(props: { id: { id: string } }) {
                 </div>
               </div>
             ))}
+            <div className={`mt-3 ${thereMoreNotes? 'flex' : 'hidden'}`}>See more...</div>
+            <div className={`w-96 ${thereNoNote? 'flex' : 'hidden'}`}>You don't have any note...</div>
             <Link
               className="absolute -right-20 flex h-full items-center"
               href={{ pathname: "/books/[id]/notes", query: { id: idBook } }}
@@ -72,7 +110,7 @@ export default function TasksBooks(props: { id: { id: string } }) {
             <h2 className="text-2xl ">Your To do</h2>
           </Link>
           <div className="flex flex-col gap-2">
-            {todoList.data?.map((todo) => (
+            {todoList.data?.slice(0,5).map((todo) => (
               <ToDoTask
                 TaskCheck={todo.check}
                 taskId={todo.id}
@@ -81,6 +119,8 @@ export default function TasksBooks(props: { id: { id: string } }) {
                 bookId={todo.bookId}
               />
             ))}
+            <div className={`mt-3 ${thereMoreTodo? 'visible' : 'invisible'}`}>See more...</div>
+            <div className={`${thereNoTodo? 'visible' : 'invisible'}`}>You don't have any to do...</div>
             <Link
               className="absolute -right-20 flex h-full items-center"
               href={{ pathname: "/books/[id]/todo", query: { id: idBook } }}
